5e39bbabafb76e4436ded7ceb292cfda3112eedd
TeaLeafEgggg <chngoscar@gmail.com>
2025-10-30 19:25:27 +0800

    Added most of the requirement

--- BFS_assignment2_working.py
+++ BFS_assignment2_working.py
@@ -16,7 +16,7 @@ class Node: #Required in the
     '''
     def __init__(self, name):
         self.name = name
-        self.neighbouts = []
+        self.neighbours = []
     
     def add_neighbour(self, neighbour_node):
         if neighbour_node not in self.neighbours: 
@@ -54,24 +54,67 @@ def main():
 # TODO: Use you created Class Node to creating Graph for Traversal
 
 #Create all Node Objects
+    nodes = {}
+    for char in 'abcdef':
+        nodes[char] = Node(char)
 
 #Use neighbour to create the graphic
+    nodes['a'].add_neighbour(nodes['d'])
+    nodes['b'].add_neighbour(nodes['d'])
+    nodes['c'].add_neighbour(nodes['b'])
+    nodes['d'].add_neighbour(nodes['e'])
+    nodes['d'].add_neighbour(nodes['f'])
+    nodes['e'].add_neighbour(nodes['a'])
+    nodes['f'].add_neighbour(nodes['b'])
 
+#Output the graphic connections
+    print("=== GRAPH CONNECTIONS ===")
+    for name, node_obj in nodes.items():
+        neigh_str = ', '.join(n.name for n in node_obj.neighbours)
+        print(f"Node {name}: → {neigh_str}")
+    print("====================================\n")
 #====================================================
 
 # TODO: geting the source and destination
 # Note: your program needs to verify validate inputs, 
 # such as node such node in the graphic, the sourse and destination are the same!
-
+    while True:
+        while True:
+            src_start = input("Enter the source node: ").strip().lower()
+            if src_start in nodes:
+                break
+            print("Invalid source node. Please try again.")
+        while True:
+            dest_end = input("Enter the destination node: ").strip().lower()
+            if dest_end in nodes:
+                break
+            print("Invalid destination node. Please try again.")
+        if src_start == dest_end:
+            print("Source and destination cannot be the same. Please try again.")
+        else:
+            break
 #=====================================================
 
 # TODO: BFS to find the path
 
+    path = BFS(nodes[src_start], nodes[dest_end])
 #=====================================================
 
 # TODO: Output the path if found
+    if path:
+        path_str = ' -> '.join(n.name for n in path)
+        print(f"Path found: {path_str}")
+    else:
+        print("NO path found between the specified nodes.")
 
 #=====================================================
+    cont = input("Will you want to continue? Y/N\n").strip().upper()
+    if cont == 'N':
+        print("System Terminate!")
+        exit()
+    print()
 
+if __name__ == "__main__":
+    main()
  
 
